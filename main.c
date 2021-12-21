a03f764e46250ba2c00007b90fcf5a5c8956c6ca
Adrian Jung <adrian.mael.jung@gmail.com>
2021-12-21 19:01:37 +0100

    push auto

--- main.c
+++ main.c
@@ -1,14 +1,67 @@
 #include "mlx/mlx.h"
 
-int main(void)
-{
-    void    *mlx;
-    // void    *window1;
-    void    *img;
 
-    mlx = mlx_init();
-    // window1 = mlx_new_window(mlx, 1920, 1080, "Test");
-    // mlx_loop(mlx);
-    img = mlx_new_image(mlx, 1920, 1080);
-    return (0);
+
+typedef struct	s_data 
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
 }
+
+void    print_square(int x, t_data *img)
+{
+    int i;
+
+    i = 0;
+    while (i < x)
+    {
+        my_mlx_pixel_put(img, 900 + i, 500, 0x00FF0000);
+        my_mlx_pixel_put(img, 900 + i, 500 + x, 0x00FF0000);
+        my_mlx_pixel_put(img, 900, 500 + i, 0x00FF0000);
+        my_mlx_pixel_put(img, 900 + x, 500 + i, 0x00FF0000);
+        i++;
+    }
+}
+
+void print_triangle(int x, t_data *img)
+{
+    int i;
+
+    i = 0;
+    while (i < x)
+    {
+        my_mlx_pixel_put(img, 900 - i / 2, 500 + i / 2, 0x00FF0000);
+        my_mlx_pixel_put(img, 900 + i / 2, 500 + i / 2, 0x00FF0000);
+        my_mlx_pixel_put(img, 900 - x / 2 + i, 500 + x / 2, 0x00FF0000);
+        i++;
+    }
+
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+    print_triangle(500, &img);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
+
