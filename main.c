de5711f305994606d9fab177d40cb106a40b9f47
Adrian JUNG <ajung@e2r4p6.clusters.42paris.fr>
2022-01-05 23:23:34 +0100

    push auto

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "mlx/mlx.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 // open, read, write, close
 // ◦ malloc, free
 // ◦ perror, strerror
@@ -18,6 +18,11 @@ typedef struct	s_data
 	int		endian;
 }				t_data;
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
 struct trgb_char
 {
     unsigned char b;
@@ -32,10 +37,15 @@ typedef union u_color
     struct trgb_char	inside;
 }    t_color;
 
-int	create_trgb(int t, int r, int g, int b)
-{
-	return (t << 24 | r << 16 | g << 8 | b);
-}
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -81,58 +91,90 @@ void    print_square_texture(int x, t_data *img)
 
 void	print_square(int x, t_data *img)
 {
-	int	i;
+	double	i;
+	double	ratio;
+	t_color couleur1;
+
+	couleur1.trgb = 0x003030FF;
 
 	i = 0;
 	while (i < x)
 	{
-		my_mlx_pixel_put(img, 800 + i, 500, 0x0000FFFF);
-		my_mlx_pixel_put(img, 800 + i, 500 + x, 0x0000FFFF);
-		my_mlx_pixel_put(img, 800, 500 + i, 0x0000FFFF);
-		my_mlx_pixel_put(img, 800 + x, 500 + i, 0x0000FFFF);
+		ratio = (i / x);
+		couleur1.inside.b = 255 + ratio * (30 - 255);
+		couleur1.inside.r = 30 + ratio * (255 - 30);
+		couleur1.inside.g++;
+		my_mlx_pixel_put(img, 800 + i, 500, couleur1.trgb);
+		my_mlx_pixel_put(img, 800 + x - i, 500 + x, couleur1.trgb);
+		my_mlx_pixel_put(img, 800, 500 + i, couleur1.trgb);
+		my_mlx_pixel_put(img, 800 + x, 500 + x - i, couleur1.trgb);
 		i++;
 	}
 }
 
 void	print_colors(int x, t_data *img)
 {
-	int		i;
-	int		j;
+	double	i;
+	double	ratio;
 	t_color couleur1;
 
 	i = 1;
-	j = 1;
-	//couleur de depart: bleu: 0x003030FF;
 	couleur1.trgb = 0x003030FF;
-	//couleur d'arrive: rouge.0x00FF3030
-	// difference entre les deux.
 	while (i < x)
 	{
-		//incrementer tous de 1% la couleur tous les 1% de x
-		if (i > (x * j / 100))
-		{
-			couleur1.trgb += j * (0x00FF3030 - 0x003030FF) / 100;
-			j++;
-		}
+		ratio = (i / x);
+		printf("%f\n", ratio);
+		couleur1.inside.b = 255 + ratio * (30 - 255);
+		couleur1.inside.r = 30 + ratio * (255 - 30);
 		my_mlx_pixel_put(img, 800 + i, 500, couleur1.trgb);
 		i++;
 	}
 	
 }
 
+int	close(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		return (0);
+	}
+	printf("keycode: %d\n", keycode);
+	return (0);
+}
+
+int	mouse_hook(int keycode, t_vars *vars)
+{
+	printf("keycode: %d\n", keycode);
+	return (0);
+}
+
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_vars	vars;
 	t_data	img;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "My window");
+
+	img.img = mlx_new_image(vars.mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	
-	print_colors(500, &img);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_hook(vars.win, ON_DESTROY, 0, close, &vars);
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_mouse_hook(vars.win, mouse_hook, &vars);
+
+	print_square(500, &img);
+
+	//mlx_string_put to write on the image
+	mlx_loop(vars.mlx);
 }
